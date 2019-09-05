bd9454e2db17e468399668198b9db9652f0a8430
Keith Rome <keith.rome@gmail.com>
2019-09-05 14:01:27 -0500

    Include randomized string in shortened names

--- infra/templates/az-service-single-region/commons.tf
+++ infra/templates/az-service-single-region/commons.tf
@@ -19,14 +19,15 @@ locals {
   app_id  = random_string.workspace_scope.keepers.app_id
   region = replace(trimspace(lower(var.resource_group_location)), "_", "-")
   ws_name = random_string.workspace_scope.keepers.ws_name
+  suffix = var.randomization_level > 0 ? "-${random_string.workspace_scope.result}" : ""
 
   // base name for resources, name constraints documented here: https://docs.microsoft.com/en-us/azure/architecture/best-practices/naming-conventions
-  base_name     = "${local.app_id}-${local.ws_name}%{ if var.randomization_level > 0 }-${random_string.workspace_scope.result}%{ endif }"
-  base_name_21  = length(local.base_name) < 22 ? local.base_name : "${substr(replace(local.app_id, "-", ""), 0, 10)}-${substr(replace(local.ws_name, "-", ""), 0, 10)}"
-  base_name_46  = length(local.base_name) < 47 ? local.base_name : "${substr(replace(local.app_id, "-", ""), 0, 25)}-${substr(replace(local.ws_name, "-", ""), 0, 20)}"
-  base_name_60  = length(local.base_name) < 61 ? local.base_name : "${substr(replace(local.app_id, "-", ""), 0, 34)}-${substr(replace(local.ws_name, "-", ""), 0, 25)}"
-  base_name_76  = length(local.base_name) < 77 ? local.base_name : "${substr(replace(local.app_id, "-", ""), 0, 45)}-${substr(replace(local.ws_name, "-", ""), 0, 30)}"
-  base_name_83  = length(local.base_name) < 84 ? local.base_name : "${substr(replace(local.app_id, "-", ""), 0, 52)}-${substr(replace(local.ws_name, "-", ""), 0, 30)}"
+  base_name     = "${local.app_id}-${local.ws_name}${local.suffix}"
+  base_name_21  = length(local.base_name) < 22 ? local.base_name : "${substr(local.base_name, 0, 21 - length(local.suffix))}${local.suffix}"
+  base_name_46  = length(local.base_name) < 47 ? local.base_name : "${substr(local.base_name, 0, 46 - length(local.suffix))}${local.suffix}"
+  base_name_60  = length(local.base_name) < 61 ? local.base_name : "${substr(local.base_name, 0, 60 - length(local.suffix))}${local.suffix}"
+  base_name_76  = length(local.base_name) < 77 ? local.base_name : "${substr(local.base_name, 0, 76 - length(local.suffix))}${local.suffix}"
+  base_name_83  = length(local.base_name) < 84 ? local.base_name : "${substr(local.base_name, 0, 83 - length(local.suffix))}${local.suffix}"
 
   // Resource names
   admin_rg_name    = "${local.base_name_83}-adm-rg"               // resource group used for admin resources (max 90 chars)
