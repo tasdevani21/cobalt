5fdc2c55cb9c2752a12fa821ccf4d71f0d771232
Dexter Williams <Dexterw@microsoft.com>
2019-09-08 18:47:39 -0500

    reconfigure construction of kv secret uri to bypass provider construction of the same

--- infra/modules/providers/azure/keyvault-secret/output.tf
+++ infra/modules/providers/azure/keyvault-secret/output.tf
@@ -1,3 +1,8 @@
+output "keyvault_secret_names" {
+  description = "The name of the Keyvault secret"
+  value       = azurerm_key_vault_secret.secret.*.name
+}
+
 output "keyvault_secret_ids" {
   description = "The id of the Keyvault secret"
   value       = azurerm_key_vault_secret.secret.*.id
